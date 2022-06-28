f04aa1e371cbdf97e1499e1f3fa136149f82d74e
wea9677 <wea9677@naver.com>
2022-06-28 21:41:10 +0900

    구글 로그인 닉네임 정보 추가

--- passport/google.js
+++ passport/google.js
@@ -32,7 +32,7 @@ module.exports = () => {
                     } else {
                         // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                         const newUser = await User.create({
-                            // nick: profile.displayName,
+                            nickName: profile.displayName,
                             userId: profile.id,
                             provider: 'google',
                         });
