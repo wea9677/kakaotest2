2f69e2c5bba966631f401ef3040417d66c16a3fe
wea9677 <wea9677@naver.com>
2022-06-29 11:49:44 +0900

    email 삭제

--- passport/naver.js
+++ passport/naver.js
@@ -25,7 +25,7 @@ module.exports = () => {
                } else {
                   // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
                   const newUser = await User.create({
-                     email: profile.email,
+                     // email: profile.email,
                      nickName: profile.name,
                      userId: profile.id,
                      userImg : profile.profileImage,
