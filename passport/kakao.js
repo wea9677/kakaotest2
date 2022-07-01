5f04e5283808afd6bf078da2cc282dfcafd9b6ca
wea9677 <wea9677@naver.com>
2022-07-01 14:58:46 +0900

    뭐라도 나와라

--- passport/kakao.js
+++ passport/kakao.js
@@ -31,15 +31,16 @@ module.exports = () => {
                   //  if (exUser) {
                      //   done(null, exUser); // 로그인 인증 완료
                    // } else {
-                        // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
-                        const newUser = await User.create({
-                            userId: profile.id,
-                            provider: 'kakao',
-                            nickName : profile._json.properties.nickname,
-                            userImg : profile._json.properties.thumbnail_image
-                        });
-                        done(null, newUser); // 회원가입하고 로그인 인증 완료
-                        console.log("가입완료")
+                        // // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
+                        // const newUser = await User.create({
+                        //     userId: profile.id,
+                        //     provider: 'kakao',
+                        //     nickName : profile._json.properties.nickname,
+                        //     userImg : profile._json.properties.thumbnail_image
+                        // });
+                        // done(null, newUser); // 회원가입하고 로그인 인증 완료
+                        const userinfo = await User.findAll({})
+                        console.log( userinfo ,"가입완료")
                     }
                 //}
                  catch (error) {
