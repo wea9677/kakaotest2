11653a5f1864e0b43890e97ce57bbfc7e944d087
wea9677 <wea9677@naver.com>
2022-07-04 11:26:27 +0900

    이거는

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const sequelize = require("sequelize")
 
 const db = new sequelize('jejudo', 'root', '1q2w3e4r!', {
         host : "127.0.0.1",
-        port : 3306,
         dialect: "mysql"
 });
 
