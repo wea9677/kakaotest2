81c043bdb540d5e08f4ed57dbb8f359cd5c0ad55
wea9677 <wea9677@naver.com>
2022-07-04 20:09:39 +0900

    잠깐만

--- passport/index.js
+++ passport/index.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
-
+const { Users } = require('../models/index');
 const passport = require('passport');
 const google = require('./google');
 const kakao = require('./kakao');
 const naver = require('./naver');
 module.exports = () => {
-    passport.serializeUser((user, done) => {
+    passport.serializeUser((Users, done) => {
         // req.login(user, ...)의 user가 넘어와 값을 이용할수 있다.
         // console.log('직렬화', user[0].userId);
-        done(null, user);
+        done(null, Users);
     });
     passport.deserializeUser((user, done) => {
         // req.session에 저장된 사용자 아이디를 바탕으로 DB 조회로 사용자 정보를 얻어낸 후 req.user에 저장.
         // 즉, id를 sql로 조회해서 전체 정보를 가져오는 복구 로직이다.
-        console.log('역직렬화', user);
-        done(null, user);
+        console.log('역직렬화', Users);
+        done(null, Users);
     });
 
     // passport.serializeUser(function(user, done) {
