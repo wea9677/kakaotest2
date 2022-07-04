55acf7821a15268f2a3c6c7adbfb84f1dea0dbc1
wea9677 <wea9677@naver.com>
2022-07-04 11:20:32 +0900

    로컬db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const sequelize = require("sequelize")
 
 const db = new sequelize('jejudo', 'root', '1q2w3e4r!', {
-        host : "localhost",
+        host : "127.0.0.1",
         port : 3306,
         dialect: "mysql"
 });
