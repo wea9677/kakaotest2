6be6b1ce59f20f005d9a48e9435f578563b118a4
wea9677 <wea9677@naver.com>
2022-07-04 18:18:01 +0900

    테스트

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 'use strict';
+const path = require('path');
 const Sequelize = require('sequelize');
+
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const db = {};
+
 
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
