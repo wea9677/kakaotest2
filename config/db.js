73acc570f33d1a00bb33495f6bf17adc4ec2076f
wea9677 <wea9677@naver.com>
2022-07-04 11:10:48 +0900

    주소변경2

--- config/db.js
+++ config/db.js
@@ -1,18 +1,9 @@
-require('dotenv').config();
 const sequelize = require("sequelize")
 
-const db = new sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_USER_PASSWORD, {
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+const db = new sequelize('jejudo', 'root', '1q2w3e4r!', {
+        host : "13.125.112.232",
+        dialect: "mysql"
 });
-
 
 db.sync({});
 
