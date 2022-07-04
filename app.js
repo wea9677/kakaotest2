eb03b201801bdc4c1aef493d6193a840f4bd58f8
wea9677 <wea9677@naver.com>
2022-07-04 21:01:16 +0900

    잠깐꺼

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ const app = express()
 
 
 // MySQL
-const db = require("./models");
-db.sequelize
-  .sync()
-  .then(() => {
-    // console.log("MySQL DB 연결 성공");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// const db = require("./models");
+// db.sequelize
+//   .sync()
+//   .then(() => {
+//     // console.log("MySQL DB 연결 성공");
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 // const httpsPort = process.env.HTTPS_PORT
 
